db730f5774be2c38813069ab6ad2ef5069bd3d0b
Nelson Durrant <snelsondurrant@gmail.com>
2025-02-11 10:20:14 -0700

    Fix for bridge speed

--- scripts/run_bridge.sh
+++ scripts/run_bridge.sh
@@ -13,4 +13,4 @@ source ~/ros1_msgs_ws/install_isolated/setup.bash
 source ~/ros2_humble/install/setup.bash
 source ~/ros2_msgs_ws/install/local_setup.bash
 source ~/bridge_ws/install/local_setup.bash
-ros2 run ros1_bridge dynamic_bridge --bridge-all-topics
+ros2 run ros1_bridge dynamic_bridge
